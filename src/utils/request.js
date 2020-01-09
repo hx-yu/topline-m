c7b89e8c22bf76c2feb8ab7b45b1ae94b054e9db
yuhexin <yuhexin@git.com>
2020-01-09 21:18:59 +0800

    处理请求注入token

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,6 +1,7 @@
 // 此文件是配置axios文件
 import axios from 'axios'
 import jsonBig from 'json-bigint'
+import store from '@/store'
 // 配置基地址
 const request = axios.create({
   baseURL: 'http://ttapi.research.itcast.cn/app/v1_0'
@@ -16,10 +17,18 @@ request.defaults.transformResponse = [data => {
 // 设置请求拦截
 // 处理token
 request.interceptors.request.use(config => {
+  const { user } = store.state
+  if (user) {
+    config.headers.Authorization = `Bearer ${user.token}`
+  }
   return config
 }, error => {
   return Promise.reject(error)
 })
 // 设置响应拦截
-
+request.interceptors.response.use(response => {
+  console.log(response)
+}, error => {
+  console.log(error)
+})
 export default request
